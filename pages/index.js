fe7d9a679854418e25152af3e5c0dbdb9c6d515e
aleray <adrienleray131295@gmail.com>
2022-09-29 20:15:55 +0200

    ♻️ refact: remove old config

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Home.module.scss'
-
-export default function Home() {
-  return (
-    <div className={styles.wip}>
-      <h1>Work in progess ! 🔧</h1>
-    </div>
-  )
-}
