b4ab8e2568a0785723c3925f6bc34c48bf1ef0d2
Adrien Leray <adrienleray131295@gmail.com>
2022-10-10 22:57:11 +0200

    ✨ feat: add base template

--- _config.ts
+++ _config.ts
@@ -1,12 +1,9 @@
 import lume from "lume/mod.ts";
-import md from "https://jspm.dev/markdown-it-container";
 
 const site = lume({
   src: "./src",
-}, {
-  markdown: {
-    plugins: [md],
-  },
 });
+
+site.copy("favicon.ico");
 
 export default site;
